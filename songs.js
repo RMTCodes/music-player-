5f2cd36313a4525b5773edf539ea8c3a992b9bfc
RMTCodes <rafaela.tokai@aluno.faculdadeimpacta.com.br>
2023-08-29 20:34:49 -0300

    4° Commit - Adicionando JavaScript

---
+++ songs.js
@@ -0,0 +1,35 @@
+/*export defaut [
+    { 
+        src: "./YouMusic/deadman.mp3", 
+        name: "Deadman music", 
+    },
+    { 
+        src: "./YouMusic/hotmilk.mp3", 
+        name: "Hotmilk music", 
+    },
+
+    {
+         scr: "./YouMusic/Shut Me Up.mp3", 
+         name: "Shut Me Up",
+    },
+
+];*/
+
+const links = [
+    { 
+        src: "./YouMusic/deadman.mp3", 
+        name: "Deadman", 
+    },
+    { 
+        src: "./YouMusic/hotmilk.mp3", 
+        name: "Hotmilk", 
+    },
+
+    {
+         src: "./YouMusic/Shut Me Up.mp3", 
+         name: "Shut Me Up",
+    },
+
+];
+  
+  export default links;
